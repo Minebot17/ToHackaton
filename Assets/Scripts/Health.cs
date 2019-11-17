128b5d6fbe10fd1622e3d02b98254427d52bc2a7
Eugene <serpinin@mail.ru>
2019-11-17 11:14:00 +0400

    hp bar

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Health : MonoBehaviour {
     public int health;
     public int maxHealth;
+    public GameObject healthBar;
+    public bool isPlayer;
+    private long lastDamage;
+
+    public void Start() {
+        UpdateHealthBar(health);
+    }
 
     public int Heal(int heal) {
         int preHealth = health;
         health += heal;
         if (health > maxHealth)
             health = maxHealth;
+        UpdateHealthBar(health);
         return health - preHealth;
     }
 
@@ -22,6 +31,25 @@ public class Health : MonoBehaviour {
             if (GetComponent<Death>() != null)
                 GetComponent<Death>().OnDeath();
         }
+        UpdateHealthBar(health);
         return health - preDamage;
+    }
+
+    private void UpdateHealthBar(int newHealth) {
+        if (healthBar != null) {
+            healthBar.transform.GetChild(0).GetComponent<SpriteRenderer>().size = new Vector2((int)(newHealth/(float)maxHealth*62f), 6);
+            if (!isPlayer) {
+                healthBar.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+                lastDamage = System.DateTime.Now.Ticks;
+                Invoke(nameof(HideHealthBar), 2f);
+            }
+            else
+                healthBar.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = true;
+        }
+    }
+
+    private void HideHealthBar() {
+        if (System.DateTime.Now.Ticks - lastDamage > 15000000)
+            healthBar.transform.GetChild(0).GetComponent<SpriteRenderer>().enabled = false;
     }
 }
